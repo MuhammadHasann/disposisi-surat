b694a4efbe4884a0028bd3bff062fe96c71d95be
MuhammadHasann <muhammadhas772@gmail.com>
2023-01-20 16:34:36 +0700

    Session 3 - Add Surat Masuk

--- application/views/admin/template/header.php
+++ application/views/admin/template/header.php
@@ -6,7 +6,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
         <meta name="description" content="" />
         <meta name="author" content="" />
-        <title>Sistem Point</title>
+        <link rel="icon" href="<?= base_url('/assets/img/uniska.png') ?>">
+        <title><?= $title ?></title>
         <link href="<?= base_url('/assets/css/styles.css') ?>" rel="stylesheet"/>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
